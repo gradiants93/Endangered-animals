32a394719ccf71ca8b950af1d5945466b17c636a
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-25 11:13:11 -0700

    added hero image

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Species from "./components/species";
 import Sightings from "./components/sightings";
+import HeroSection from "./components/hero";
 
 function App() {
   // const [page, setPage] = useState("sightings")
   return (
     <div className="App">
-      Hello from Techtonica
-      <Species />
-      <Sightings />
+      <div className="hero-image">
+        <HeroSection />
+     </div>
+      {/* Hello from Techtonica */}
+      <div className="forms">
+        <Species />
+        <Sightings />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import '../App.css';
+
+function HeroSection() {
+    return (
+        <div className="hero-text">
+            <h1>Who did you see?</h1>
+            {/* <img src="http://www.mjkelly.info/images/projects/Jaguar.JPG" width={1000}></img> */}
+
+        </div>
+    )
+}
+export default HeroSection;
